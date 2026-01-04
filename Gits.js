f25d3da83045004d91ba9edff999aa342e09b647
Mohit Kumar <m2125729@gmail.com>
2026-01-04 17:50:12 +0530

    payment_2

--- Gits.js
+++ Gits.js
@@ -4,16 +4,7 @@ console.log(button);
 const login = "Login Page";
 console.log(login);
 
-<<<<<<< HEAD
 const footer = "Footer added";
 console.log(footer);
-=======
 const payment = "Payment Added";
 console.log(payment);
-<<<<<<< HEAD
->>>>>>> ca08e0a (payment1_commit)
-=======
-
-const upi = "Upi is added";
-console.log(upi);
->>>>>>> 3889d61 (payment2_commit)
