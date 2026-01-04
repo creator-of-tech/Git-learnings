8ce1c5b06c405ecaa0762f00f03d04ce2777c1ff
Mohit Kumar <m2125729@gmail.com>
2026-01-04 17:25:56 +0530

    payment1_commit

--- Gits.js
+++ Gits.js
@@ -4,5 +4,10 @@ console.log(button);
 const login = "Login Page";
 console.log(login);
 
+<<<<<<< HEAD
 const footer = "Footer added";
 console.log(footer);
+=======
+const payment = "Payment Added";
+console.log(payment);
+>>>>>>> ca08e0a (payment1_commit)
